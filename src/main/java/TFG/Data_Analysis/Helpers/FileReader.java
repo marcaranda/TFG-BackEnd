dcb004029df3e51e07d3b3af866e52a9c86f939c
marcaranda <marc.aranda@estudiantat.upc.edu>
2024-02-27 19:15:50 +0100

    inicio file lector

--- src/main/java/TFG/Data_Analysis/Helpers/FileReader.java
+++ src/main/java/TFG/Data_Analysis/Helpers/FileReader.java
@@ -1,75 +1,32 @@
 package TFG.Data_Analysis.Helpers;
 
 import com.opencsv.*;
-import com.opencsv.exceptions.CsvException;
-import org.springframework.core.io.ClassPathResource;
-import org.apache.poi.ss.usermodel.*;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.csv.CSVParser;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.List;
+import java.io.Reader;
+import java.nio.file.*;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/file")
 public class FileReader {
-    public void fileReaderExcel(String path) throws Exception, IOException {
-        ClassPathResource resource = new ClassPathResource(path);
-        InputStream inputStream = resource.getInputStream();
-        Workbook workbook = WorkbookFactory.create(inputStream);
-        Sheet sheet = workbook.getSheetAt(0);
+    @PostMapping
+    public void fileReaderCSV(@RequestBody String path) throws Exception, IOException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(path));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
+                Map<String, Integer> headerMap = csvParser.getHeaderMap();
 
-        boolean end = false;
-        Iterator<Row> rowIterator = sheet.iterator();
-        while (rowIterator.hasNext() && !end) {
-            Row row = rowIterator.next();
-
-            // Verifica si la fila está vacía (no hay más datos)
-            boolean isEmptyRow = true;
-            for (int i = 0; i < row.getLastCellNum(); i++) {
-                Cell cell = row.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                if (cell.getCellType() != CellType.BLANK) {
-                    isEmptyRow = false;
-                    break;
+                for (CSVRecord csvRecord : csvParser) {
+                    for (String columnName : headerMap.keySet()) {
+                        String columnValue = csvRecord.get("ColumnName");
+                        // Procesar los datos como se requiera
+                        System.out.println(columnValue);
+                    }
                 }
-            }
-
-            if (isEmptyRow) {
-                end = true; // Marca el fin cuando se encuentra una fila vacía
-            } else {
-                // Procesa la fila actual si no está vacía
-            }
         }
-        inputStream.close();
-    }
-
-    public void fileReaderCSV(String path) throws Exception, IOException {
-        ClassPathResource resource = new ClassPathResource(path);
-        InputStream inputStream = resource.getInputStream();
-
-        CSVParser parser = new CSVParserBuilder()
-                .withSeparator(',') // Define el separador (coma en este caso)
-                .withIgnoreQuotations(false) // Opcional: considera comillas como caracteres especiales
-                .build();
-
-        try (CSVReader csvReader = new CSVReaderBuilder(new InputStreamReader(inputStream))
-                .withCSVParser(parser)
-                .build()) {
-
-            List<String[]> allData = csvReader.readAll();
-
-            for (String[] row : allData) {
-                // Aquí puedes procesar cada fila del archivo CSV
-                // Cada fila es un array de strings que contiene los valores de las columnas
-                // Por ejemplo, si tienes columnas "Nombre", "Edad", "Ciudad", puedes acceder a los valores así:
-                // String nombre = row[0];
-                // String edad = row[1];
-                // String ciudad = row[2];
-            }
-        } catch (CsvException e) {
-            throw new RuntimeException(e);
-        }
-        inputStream.close();
     }
 }
