c84da2625b2212ab74e5ddd04024c20efc57d442
marcaranda <marc.aranda@estudiantat.upc.edu>
2024-03-13 20:01:07 +0100

    calculo entropy mejora persistencia de datos

--- src/main/java/TFG/Data_Analysis/Service/DatasetService.java
+++ src/main/java/TFG/Data_Analysis/Service/DatasetService.java
@@ -5,28 +5,38 @@ import TFG.Data_Analysis.Repository.DatasetRepo;
 import TFG.Data_Analysis.Repository.Entity.DatasetEntity;
 import TFG.Data_Analysis.Security.TokenValidator;
 import TFG.Data_Analysis.Service.Model.DatasetModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.bson.types.ObjectId;
 import org.ejml.dense.row.decomposition.eig.SwitchingEigenDecomposition_DDRM;
 import org.ejml.dense.row.factory.DecompositionFactory_DDRM;
 import org.ejml.simple.SimpleMatrix;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class DatasetService {
     @Autowired
     DatasetRepo datasetRepo;
     @Autowired
-    UserService userService;
+    GridFsTemplate gridFsTemplate;
 
     public DatasetModel fileReader(MultipartFile file, long userId) throws Exception {
         if(new TokenValidator().validate_id_with_token(userId)) {
@@ -38,18 +48,20 @@ public class DatasetService {
                  CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
                 Map<String, Integer> headerMap = csvParser.getHeaderMap();
 
-                Map<Integer, Map<Integer, Pair<String, Double>>> dataset = new HashMap<>();
+                Map<Integer, Map<Integer, Pair<String, String>>> dataset = new HashMap<>();
                 int numRow = 1;
 
                 for (CSVRecord csvRecord : csvParser) {
-                    Map<Integer, Pair<String, Double>> row = new HashMap<>();
+                    Map<Integer, Pair<String, String>> row = new HashMap<>();
                     int numColumn = 1;
 
-                    for (String columnName : headerMap.keySet()) {
-                        String columnValueString = csvRecord.get(columnName);
-                        Double columnValue = Double.valueOf(columnValueString.replaceAll("[^0-9]", ""));
+                    for (Map.Entry<String, Integer> entry : csvParser.getHeaderMap().entrySet().stream()
+                            .sorted(Map.Entry.comparingByValue())
+                            .collect(Collectors.toList())) {
+                        String columnName = entry.getKey();
+                        String columnValue = csvRecord.get(columnName);
                         // Procesar los datos como se requiera
-                        Pair<String, Double> rowValue = new Pair<>(columnName, columnValue);
+                        Pair<String, String> rowValue = new Pair<>(columnName, columnValue);
                         row.put(numColumn, rowValue);
                         ++numColumn;
                     }
@@ -81,20 +93,20 @@ public class DatasetService {
             response.setContentType("text/csv");
             response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
 
-            Map<Integer, Map<Integer, Pair<String, Double>>> downloadDataset = dataset.getDataset();
+            Map<Integer, Map<Integer, Pair<String, String>>> downloadDataset = dataset.getDataset();
 
             try (PrintWriter csvWriter = response.getWriter()) {
                 // Escribir encabezados de columnas
-                Map<Integer, Pair<String, Double>> firstRow = downloadDataset.get(1);
-                for (Map.Entry<Integer, Pair<String, Double>> entry : firstRow.entrySet()) {
+                Map<Integer, Pair<String, String>> firstRow = downloadDataset.get(1);
+                for (Map.Entry<Integer, Pair<String, String>> entry : firstRow.entrySet()) {
                     csvWriter.append(entry.getValue().getColumn());
                     csvWriter.append(",");
                 }
                 csvWriter.append("\n");
 
                 // Escribir datos
-                for (Map<Integer, Pair<String, Double>> row : downloadDataset.values()) {
-                    for (Pair<String, Double> rowData : row.values()) {
+                for (Map<Integer, Pair<String, String>> row : downloadDataset.values()) {
+                    for (Pair<String, String> rowData : row.values()) {
                         csvWriter.append(String.valueOf(rowData.getValue()));
                         csvWriter.append(",");
                     }
@@ -116,7 +128,11 @@ public class DatasetService {
             ModelMapper modelMapper = new ModelMapper();
             List<DatasetModel> history = new ArrayList<>();
 
-            datasetRepo.findAllByUserId(userId).forEach(elementB -> history.add(modelMapper.map(elementB, DatasetModel.class)));
+            datasetRepo.findAllByUserId(userId).forEach(elementB -> {
+                DatasetModel datasetModel = modelMapper.map(elementB, DatasetModel.class);
+                datasetModel.setDataset(getDatasetMap(datasetModel.getFileIds()));
+                history.add(datasetModel);
+            });
             return history;
         }
         else {
@@ -126,16 +142,16 @@ public class DatasetService {
 
     public double applyFilter(List<String> filter, long userId, String datasetName, int version) throws Exception {
         if(new TokenValidator().validate_id_with_token(userId)) {
-            Map<Integer, Map<Integer, Pair<String, Double>>> newDataset = new HashMap<>();
-            Map<Integer, Map<Integer, Pair<String, Double>>> originalDataset = getDataset(userId, datasetName, version).getDataset();
+            Map<Integer, Map<Integer, Pair<String, String>>> newDataset = new HashMap<>();
+            Map<Integer, Map<Integer, Pair<String, String>>> originalDataset = getDataset(userId, datasetName, version).getDataset();
 
             int numRow = 1;
-            for (Map<Integer, Pair<String, Double>> entry : originalDataset.values()) {
-                Map<Integer, Pair<String, Double>> row = new HashMap<>();
+            for (Map<Integer, Pair<String, String>> entry : originalDataset.values()) {
+                Map<Integer, Pair<String, String>> row = new HashMap<>();
                 int numColumn = 1;
-                for (Pair<String, Double> subEntry : entry.values()) {
+                for (Pair<String, String> subEntry : entry.values()) {
                     if (filter.contains(subEntry.getColumn())) {
-                        Pair<String, Double> rowValue = new Pair<>(subEntry.getColumn(), subEntry.getValue());
+                        Pair<String, String> rowValue = new Pair<>(subEntry.getColumn(), subEntry.getValue());
                         row.put(numColumn, rowValue);
                         ++numColumn;
                     }
@@ -153,7 +169,7 @@ public class DatasetService {
         }
     }
 
-    private double calculateEigenEntropy(Map<Integer, Map<Integer, Pair<String, Double>>> dataset) {
+    private double calculateEigenEntropy(Map<Integer, Map<Integer, Pair<String, String>>> dataset) {
         SimpleMatrix dataMatrix = new SimpleMatrix(convertToMatrix(dataset));
         SwitchingEigenDecomposition_DDRM eigDecomp = (SwitchingEigenDecomposition_DDRM) DecompositionFactory_DDRM.eig(dataMatrix.numRows(), true);
         eigDecomp.decompose(dataMatrix.getMatrix());
@@ -177,7 +193,7 @@ public class DatasetService {
         return eigenEntropy;
     }
 
-    private double[][] convertToMatrix (Map<Integer, Map<Integer, Pair<String, Double>>> dataset) {
+    private double[][] convertToMatrix (Map<Integer, Map<Integer, Pair<String, String>>> dataset) {
         int numRows = dataset.size();
         int numColumns = dataset.isEmpty() ? 0 : dataset.entrySet().iterator().next().getValue().size() - 1;
 
@@ -190,22 +206,17 @@ public class DatasetService {
         }
 
         int row = 0;
-        for (Map.Entry<Integer, Map<Integer, Pair<String, Double>>> entry : dataset.entrySet()) {
-            Map<Integer, Pair<String, Double>> rowsValues = entry.getValue();
-            boolean firstColumn = true;
-            for (Map.Entry<Integer, Pair<String, Double>> subEntry : rowsValues.entrySet()) {
-                Pair<String, Double> value = subEntry.getValue();
-                int column = 0;
-                if (!firstColumn) {
+        for (Map<Integer, Pair<String, String>> entry : dataset.values()) {
+            int column = 0;
+            for (Pair<String, String> value : entry.values()) {
+                if (column > 0) {
                     if (value.getValue() != null) {
-                        dataMatrix[row][column] = value.getValue();
+                        dataMatrix[row][column - 1] = Double.parseDouble(value.getValue());
                     } else {
-                        dataMatrix[row][column] = 0.0;
+                        dataMatrix[row][column - 1] = 0.0;
                     }
-                    ++column;
-                } else {
-                    firstColumn = false;
                 }
+                ++column;
             }
             ++row;
         }
@@ -280,7 +291,7 @@ public class DatasetService {
     //endregion
 
     //region DataBase
-    private DatasetModel saveDataset(Map<Integer, Map<Integer, Pair<String, Double>>> dataset, double eigenEntropy, long userId, String datasetName){
+    private DatasetModel saveDataset(Map<Integer, Map<Integer, Pair<String, String>>> dataset, double eigenEntropy, long userId, String datasetName) throws JsonProcessingException {
         ModelMapper modelMapper = new ModelMapper();
 
         List<DatasetModel> datasetsVersions = new ArrayList<>();
@@ -296,26 +307,21 @@ public class DatasetService {
 
         long datasetId = autoIncrementId();
 
-        DatasetModel datasetModel = new DatasetModel(datasetId, dataset, eigenEntropy, userId, datasetName, version);
+        List<ObjectId> fileIds = saveDatasetMap(dataset, datasetId);
+
+        DatasetModel datasetModel = new DatasetModel(datasetId, dataset, fileIds, eigenEntropy, userId, datasetName, version);
         datasetRepo.save(modelMapper.map(datasetModel, DatasetEntity.class));
+
         return datasetModel;
-    }
-
-    private long autoIncrementId() {
-        ModelMapper modelMapper = new ModelMapper();
-        List<DatasetModel> userDatasets = new ArrayList<>();
-
-        datasetRepo.findAll().forEach(elementB -> userDatasets.add(modelMapper.map(elementB, DatasetModel.class)));
-
-        return userDatasets.isEmpty() ? 1 :
-                userDatasets.stream().max(Comparator.comparing(DatasetModel::getDatasetId)).get().getDatasetId() + 1;
     }
 
     public DatasetModel getDataset(long userId, String datasetName, Integer version) throws Exception {
         if(new TokenValidator().validate_id_with_token(userId)) {
             ModelMapper modelMapper = new ModelMapper();
 
-            return modelMapper.map(datasetRepo.findByUserIdAndDatasetNameAndVersion(userId, datasetName, version), DatasetModel.class);
+            DatasetModel datasetModel = modelMapper.map(datasetRepo.findByUserIdAndDatasetNameAndVersion(userId, datasetName, version), DatasetModel.class);
+            datasetModel.setDataset(getDatasetMap(datasetModel.getFileIds()));
+            return datasetModel;
         }
         else {
             throw new Exception("El user_id enviado es diferente al especificado en el token");
@@ -327,11 +333,85 @@ public class DatasetService {
             ModelMapper modelMapper = new ModelMapper();
 
             DatasetModel datasetModel = modelMapper.map(datasetRepo.findByUserIdAndDatasetNameAndVersion(userId, datasetName, version), DatasetModel.class);
+            deleteDatasetMap(datasetModel.getFileIds());
             datasetRepo.delete(modelMapper.map(datasetModel, DatasetEntity.class));
         }
         else {
             throw new Exception("El user_id enviado es diferente al especificado en el token");
         }
+    }
+    //endregion
+
+    //region Private Methods
+    private long autoIncrementId() {
+        ModelMapper modelMapper = new ModelMapper();
+        List<DatasetModel> userDatasets = new ArrayList<>();
+
+        datasetRepo.findAll().forEach(elementB -> userDatasets.add(modelMapper.map(elementB, DatasetModel.class)));
+
+        return userDatasets.isEmpty() ? 1 :
+                userDatasets.stream().max(Comparator.comparing(DatasetModel::getDatasetId)).get().getDatasetId() + 1;
+    }
+
+    private List<ObjectId> saveDatasetMap (Map<Integer, Map<Integer, Pair<String, String>>> dataset, long datasetId) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(dataset);
+        byte[] datasetBytes = json.getBytes(StandardCharsets.UTF_8);
+
+        final int MAX_SIZE = 16 * 1024 * 1024; //16MB
+        int length = datasetBytes.length;
+        int offset = 0;
+        List<ObjectId> fileIds = new ArrayList<>();
+
+        while (offset < length) {
+            int chunkSize = Math.min(length - offset, MAX_SIZE);
+            byte[] chunk = Arrays.copyOfRange(datasetBytes, offset, offset + chunkSize);
+
+            ObjectId fileId = gridFsTemplate.store(new ByteArrayInputStream(chunk), Long.toString(datasetId) + "-" + fileIds.size());
+            fileIds.add(fileId);
+
+            offset += chunkSize;
+        }
+
+
+        return fileIds;
+    }
+
+    private Map<Integer, Map<Integer, Pair<String, String>>> getDatasetMap (List<ObjectId> fileIds) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            List<GridFSFile> files = new ArrayList<>();
+            for (ObjectId fileId : fileIds) {
+                GridFSFile gridFSFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(fileId)));
+                if (gridFSFile != null) {
+                    files.add(gridFSFile);
+                }
+            }
+
+            for (GridFSFile file : files) {
+                GridFsResource resource = gridFsTemplate.getResource(file);
+                InputStream inputStream = resource.getInputStream();
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    baos.write(buffer, 0, bytesRead);
+                }
+            }
+
+            String json = baos.toString(StandardCharsets.UTF_8);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            TypeReference<Map<Integer, Map<Integer, Pair<String, String>>>> typeRef = new TypeReference<>() {
+            };
+            return objectMapper.readValue(json, typeRef);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void deleteDatasetMap(List<ObjectId> fileIds) {
+        gridFsTemplate.delete(new Query(Criteria.where("_id").is(fileIds)));
     }
     //endregion
 }
--- src/main/java/TFG/Data_Analysis/Service/Model/DatasetModel.java
+++ src/main/java/TFG/Data_Analysis/Service/Model/DatasetModel.java
@@ -1,13 +1,16 @@
 package TFG.Data_Analysis.Service.Model;
 
 import TFG.Data_Analysis.Helpers.Pair;
+import org.bson.types.ObjectId;
 
+import java.util.List;
 import java.util.Map;
 
 public class DatasetModel {
     //region Dataset Attribute
     private long datasetId;
-    private Map<Integer, Map<Integer, Pair<String, Double>>> dataset;
+    private Map<Integer, Map<Integer, Pair<String, String>>> dataset;
+    private List<ObjectId> fileIds;
     private double eigenEntropy;
     private long userId;
     private String datasetName;
@@ -18,9 +21,10 @@ public class DatasetModel {
     public DatasetModel() {
     }
 
-    public DatasetModel(long datasetId, Map<Integer, Map<Integer, Pair<String, Double>>> dataset, double eigenEntropy, long userId, String datasetName, long version) {
+    public DatasetModel(long datasetId, Map<Integer, Map<Integer, Pair<String, String>>> dataset, List<ObjectId> fileIds, double eigenEntropy, long userId, String datasetName, long version) {
         this.datasetId = datasetId;
         this.dataset = dataset;
+        this.fileIds = fileIds;
         this.eigenEntropy = eigenEntropy;
         this.userId = userId;
         this.datasetName = datasetName;
@@ -37,12 +41,20 @@ public class DatasetModel {
         this.datasetId = datasetId;
     }
 
-    public Map<Integer, Map<Integer, Pair<String, Double>>> getDataset() {
+    public Map<Integer, Map<Integer, Pair<String, String>>> getDataset() {
         return dataset;
     }
 
-    public void setDataset(Map<Integer, Map<Integer, Pair<String, Double>>> dataset) {
+    public void setDataset(Map<Integer, Map<Integer, Pair<String, String>>> dataset) {
         this.dataset = dataset;
+    }
+
+    public List<ObjectId> getFileIds() {
+        return fileIds;
+    }
+
+    public void setFileIds(List<ObjectId> fileIds) {
+        this.fileIds = fileIds;
     }
 
     public double getEigenEntropy() {
